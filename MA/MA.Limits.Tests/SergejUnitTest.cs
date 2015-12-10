65f4d0717b7bea76cd6654cc982b1447bdab9137
sergiofilon <sergio.filon@gmail.com>
2015-12-10 10:19:44 +0200

    Failing UT

--- MA/MA.Limits.Tests/SergejUnitTest.cs
+++ MA/MA.Limits.Tests/SergejUnitTest.cs
@@ -1,7 +1,10 @@
 ﻿using System;
-using System.Text;
 using System.Collections.Generic;
+using FluentAssertions;
+using MA.Limits.Helpers;
+using MA.Limits.LimitsDomain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 
 namespace MA.Limits.Tests
 {
@@ -59,11 +62,115 @@ namespace MA.Limits.Tests
         #endregion
 
         [TestMethod]
-        public void TestMethod1()
+        //x->0 (1+x*sinx-cos2x)/sin^2(x)
+        public void Kuznecov_15_2()
         {
-            //
-            // TODO: Add test logic here
-            //
+            var numerator = new List<Summand>
+            {
+                new Summand
+                {
+                    Coefficient=1.0
+                },
+
+                new Summand
+                {
+                  Coefficient=1.0,
+                  Multiplicands = new List<IElementaryFunction>
+                  {
+                      new Sine { Aparam = 1.0, Bparam=0}
+                  },
+                  PolynomialDegree = 1
+                },
+
+                new Summand
+                {
+                    Coefficient = -1.0,
+                    Multiplicands = new List<IElementaryFunction>
+                    {
+                        new Cosine { Aparam = 2.0, Bparam = 0}
+                    }
+                }
+            };
+
+            var denominator = new List<Summand>
+            {
+                new Summand
+                {
+                    SumsRaisedToPower = new List<SumRaisedToPower>
+                    {
+                        new SumRaisedToPower
+                        {
+                            Degree = 2,
+                            Sum = new List<Summand>
+                            {
+                                new Summand
+                                {
+                                    Multiplicands = new List<IElementaryFunction>
+                                    {
+                                        new Sine {Aparam = 1.0, Bparam = 0}
+                                    }
+                                }
+                            }
+
+                        }
+
+                    }
+                }
+            };
+
+
+            var normalizedFunction = new NormalizedFunction
+            {
+                Numerator = numerator,
+                Denominator = denominator
+            };
+
+            var result = LimitCalculator.CalculateLimit(normalizedFunction, 0);
+
+            result.LimitResultType.Should().Be(LimitResultType.RealNumber);
+            result.Value.Should().Be(3.0);
+        }
+        [TestMethod]
+        // x->-1 (x^3+1)/sin(x+1)
+        public void Kuznecov_15_3()
+        {
+            var numerator = new List<Summand>
+            {
+                new Summand
+                {
+                    Coefficient = 1.0,
+                    PolynomialDegree = 3
+                },
+                new Summand
+                {
+                    Coefficient = 1.0,
+
+                },
+
+            };
+
+            var denominator = new List<Summand>
+            {
+                new Summand
+                {
+                    Coefficient=1.0,
+                    Multiplicands = new List<IElementaryFunction>
+                    {
+                        new Sine { Aparam = 1.0, Bparam = 1.0}
+                    }
+                }
+            };
+
+            var normalizedFunction = new NormalizedFunction
+            {
+                Numerator = numerator,
+                Denominator = denominator
+            };
+
+            var result = LimitCalculator.CalculateLimit(normalizedFunction, -1);
+
+            result.LimitResultType.Should().Be(LimitResultType.RealNumber);
+            result.Value.Should().Be(3.0);
         }
     }
 }
