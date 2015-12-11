fff81fb2624947dca67b5e5d531ce1a7f0f33cda
redips789 <povilas.jegelevicius@gmail.com>
2015-12-11 12:19:57 +0200

    Updated unit test

--- MA/MA.Limits.Tests/PoviloUnitTest.cs
+++ MA/MA.Limits.Tests/PoviloUnitTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using System.Text;
 using System.Collections.Generic;
+using FluentAssertions;
+using MA.Limits.Helpers;
+using MA.Limits.LimitsDomain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MA.Limits.Tests
@@ -11,59 +13,203 @@ namespace MA.Limits.Tests
     [TestClass]
     public class PoviloUnitTest
     {
-        public PoviloUnitTest()
+        [TestMethod]
+        // (lim x->0) sin(5x) / x = 5
+        public void CalculatePoviloLimit1()
         {
-            //
-            // TODO: Add constructor logic here
-            //
-        }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
+            var numerator = new List<Summand>
             {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
+                new Summand
+                {
+                    Coefficient = 1.0,
+                    Multiplicands = new List<IElementaryFunction>
+                    {
+                        new Sine {Aparam = 5, Bparam = 0}
+                    }
+                }
+            };
 
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
+            var denominator = new List<Summand>
+            {
+                new Summand
+                {
+                    Coefficient = 1.0,
+                    PolynomialDegree = 1,
+                }
+            };
+
+            var normalizedFunction = new NormalizedFunction
+            {
+                Numerator = numerator,
+                Denominator = denominator
+            };
+
+            var result = LimitCalculator.CalculateLimit(normalizedFunction, 0);
+
+            result.LimitResultType.Should().Be(LimitResultType.RealNumber);
+            result.Value.Should().Be(5.0);
+        }
 
         [TestMethod]
-        public void TestMethod1()
+        // (lim x->1) 5x-x^0.5 = 4
+        public void CalculatePoviloLimit2()
         {
-            //
-            // TODO: Add test logic here
-            //
+            var numerator = new List<Summand>
+            {
+                new Summand
+                {
+                    Coefficient = -1.0,
+                    Multiplicands = new List<IElementaryFunction>
+                    {
+                        new PowerFunction { Aparam = 1, Bparam = 0, PowerNumerator = 1, PowerDenominator = 2}
+                    }
+                },
+                new Summand
+                {
+                    Coefficient = 5.0,
+                    PolynomialDegree = 1.0,
+                }
+            };
+
+            var denominator = new List<Summand>
+            {
+                new Summand
+                {
+                    Coefficient = 1
+                }
+            };
+
+            var normalizedFunction = new NormalizedFunction
+            {
+                Numerator = numerator,
+                Denominator = denominator
+            };
+
+            var result = LimitCalculator.CalculateLimit(normalizedFunction, 1, 10);
+
+            result.LimitResultType.Should().Be(LimitResultType.RealNumber);
+            result.Value.Should().Be(4);
         }
+
+        [TestMethod]
+        // (lim x->-1)(x^2+6x+5) / (x^3-1) = 0
+        public void CalculatePoviloLimit3()
+        {
+            var numerator = new List<Summand>
+            {
+                new Summand
+                {
+                    Coefficient = 1.0,
+                    PolynomialDegree = 2
+                },
+                new Summand
+                {
+                    Coefficient = 6.0,
+                    PolynomialDegree = 1
+                },
+                new Summand
+                {
+                    Coefficient = 5.0
+                }
+            };
+
+            var denominator = new List<Summand>
+            {
+                new Summand
+                {
+                    Coefficient = 1.0,
+                    PolynomialDegree = 3
+                },
+                new Summand
+                {
+                    Coefficient = -1.0
+                }
+            };
+
+            var normalizedFunction = new NormalizedFunction
+            {
+                Numerator = numerator,
+                Denominator = denominator
+            };
+
+            var result = LimitCalculator.CalculateLimit(normalizedFunction, -1.0);
+
+            result.LimitResultType.Should().Be(LimitResultType.RealNumber);
+            result.Value.Should().Be(0.0);
+        }
+
+        [TestMethod]
+        // (lim x->1) sin(x-1) = 0
+        public void CalculatePoviloLimit4()
+        {
+            var numerator = new List<Summand>
+            {
+                new Summand
+                {
+                    Coefficient = 1.0,
+                    Multiplicands = new List<IElementaryFunction>
+                    {
+                       new Sine {Aparam = 1, Bparam = -1 }
+                    }
+                }
+
+            };
+
+            var denominator = new List<Summand>
+            {
+                new Summand
+                {
+                    Coefficient = 1.0,
+                }
+            };
+
+            var normalizedFunction = new NormalizedFunction
+            {
+                Denominator = denominator,
+                Numerator = numerator,
+            };
+
+            var result = LimitCalculator.CalculateLimit(normalizedFunction, 1);
+            result.LimitResultType.Should().Be(LimitResultType.RealNumber);
+            result.Value.Should().Be(0.0);
+        }
+
+        [TestMethod]
+        // (lim x->-infinity) (x^2+x) = +infinity
+        public void CalculatePoviloLimit5()
+        {
+            var numerator = new List<Summand>
+            {
+                new Summand
+                {
+                    Coefficient = 1,
+                    PolynomialDegree = 2,
+                },
+
+                new Summand
+                {
+                    Coefficient = 1,
+                    PolynomialDegree = 1,
+                }
+            };
+            var denominator = new List<Summand>
+            {
+                new Summand
+                {
+                    Coefficient = 1
+                }
+            };
+
+            var normalizedFunction = new NormalizedFunction
+            {
+                Denominator = denominator,
+                Numerator = numerator,
+            };
+
+            var result = LimitCalculator.CalculateLimit(normalizedFunction, 10);
+            result.LimitResultType.Should().Be(LimitResultType.RealNumber);
+            result.Value.Should().Be(110.0);
+        }
+
     }
 }
