3003c93a2d8c874011a439d8f1b52e1159b6d02a
redips789 <povilas.jegelevicius@gmail.com>
2015-12-13 15:08:15 +0200

    Added new test for parsing

--- MA/MA.Limits.Tests/PoviloUnitTest.cs
+++ MA/MA.Limits.Tests/PoviloUnitTest.cs
@@ -14,48 +14,10 @@ namespace MA.Limits.Tests
     [TestClass]
     public class PoviloUnitTest
     {
-        [TestMethod]
-        // (lim x->0) sin(5x) / x = 5
-        public void CalculatePoviloLimit1_StringParse()
-        {
-            string numerator = "sin(5*x)";
-            string denominator = "x";
-            var normalizedFunction = new NormalizedFunction
-            {
-                Numerator = StringToSummand.Parse(numerator),
-                Denominator = StringToSummand.Parse(denominator)
-            };
-            normalizedFunction.Numerator.Count.Should().Be(1);
-            normalizedFunction.Denominator.Count.Should().Be(1);
-            normalizedFunction.Numerator.ToArray().First().Coefficient.Should().Be(1);
-            normalizedFunction.Numerator.ToArray().First().PolynomialDegree.Should().Be(0);
-            normalizedFunction.Numerator.ToArray().First().Multiplicands.ToArray().Length.Should().Be(1);
-            normalizedFunction.Numerator.ToArray().First().Multiplicands.ToArray().First().Aparam.Should().Be(5);
-            normalizedFunction.Denominator.ToArray().First().PolynomialDegree.Should().Be(1);
-            normalizedFunction.Denominator.ToArray().First().Coefficient.Should().Be(1);
-            var result = LimitCalculator.CalculateLimit(normalizedFunction, 0);
-            result.LimitResultType.Should().Be(LimitResultType.RealNumber);
-            result.Value.Should().Be(5.0);
-        }
-
-        [TestMethod]
-        public void CalculatePoviloLimit2_StringParse()
-        {
-            string numerator = "5*x-x^(1/2)";
-            List<Summand> summands = StringToSummand.Parse(numerator);
-            summands.First().Coefficient.Should().Be(5);
-            summands.First().PolynomialDegree.Should().Be(1);
-            summands[1].Coefficient.Should().Be(-1);
-            var i = summands[1].PolynomialDegree;
-            summands[1].PolynomialDegree.Should().Be(1);
-            summands[1].Multiplicands[0].Aparam.Should().Be(1);
-            summands[1].Multiplicands[0].Bparam.Should().Be(0);
-            //summands[1].
-            summands.Count.Should().Be(2);
-        }
+        
         [TestMethod]
         // (lim x->1) 5x-x^0.5 = 4
-        public void CalculatePoviloLimit2()
+        public void CalculatePoviloLimit1()
         {
             var numerator = new List<Summand>
             {
@@ -96,7 +58,7 @@ namespace MA.Limits.Tests
 
         [TestMethod]
         // (lim x->-1)(x^2+6x+5) / (x^3-1) = 0
-        public void CalculatePoviloLimit3()
+        public void CalculatePoviloLimit2()
         {
             var numerator = new List<Summand>
             {
@@ -143,7 +105,7 @@ namespace MA.Limits.Tests
 
         [TestMethod]
         // (lim x->1) sin(x-1) = 0
-        public void CalculatePoviloLimit4()
+        public void CalculatePoviloLimit3()
         {
             var numerator = new List<Summand>
             {
@@ -179,7 +141,7 @@ namespace MA.Limits.Tests
 
         [TestMethod]
         // (lim x->-infinity) (x^2+x) = +infinity
-        public void CalculatePoviloLimit5()
+        public void CalculatePoviloLimit4()
         {
             var numerator = new List<Summand>
             {
---
+++ MA/MA.Limits.Tests/StringParserPoviloUnitTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using MA.Limits.Helpers;
+using MA.Limits.LimitsDomain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MA.Limits.Tests
+{
+    /// <summary>
+    /// Summary description for PoviloUnitTest
+    /// </summary>
+    [TestClass]
+    public class StringParserPoviloUnitTest
+    {
+        [TestMethod]
+        public void StringParse1()
+        {
+            string numeratorStr = "sin(5*x)";
+            List<Summand> numerator = StringToSummand.Parse(numeratorStr);
+            numerator.Count.Should().Be(1);
+            numerator.ToArray().First().Coefficient.Should().Be(1);
+            numerator.ToArray().First().PolynomialDegree.Should().Be(0);
+            numerator.ToArray().First().Multiplicands.ToArray().Length.Should().Be(1);
+            numerator.ToArray().First().Multiplicands.ToArray().First().Aparam.Should().Be(5);
+        }
+
+        [TestMethod]
+        public void StringParse2()
+        {
+            string numeratorStr = "5*x-x^(1/2)";
+            List<Summand> numerator = StringToSummand.Parse(numeratorStr);
+            numerator.Count.Should().Be(2);
+            numerator[0].Coefficient.Should().Be(5);
+            numerator[0].PolynomialDegree.Should().Be(1);
+            numerator[1].Coefficient.Should().Be(-1);
+            numerator[1].Multiplicands.Count.Should().Be(1);
+            numerator[1].Multiplicands[0].Aparam.Should().Be(1);
+            numerator[1].Multiplicands[0].Bparam.Should().Be(0);
+            numerator[1].Multiplicands[0].Should()
+                .Match<PowerFunction>(x => x.PowerDenominator == 2 && x.PowerNumerator == 1);
+        }
+
+        [TestMethod]
+        public void StringParse3()
+        {
+            string numeratorStr = "x^2+6*x+5";
+            List<Summand> numerator = StringToSummand.Parse(numeratorStr);
+            numerator.Count.Should().Be(3);
+            numerator[0].Coefficient.Should().Be(1);
+            numerator[0].PolynomialDegree.Should().Be(2);
+            numerator[1].Coefficient.Should().Be(6); //Ant sito failina
+            numerator[1].PolynomialDegree.Should().Be(1);
+            numerator[2].Coefficient.Should().Be(1);
+            numerator[2].PolynomialDegree.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void StringParse4()
+        {
+            string numeratorStr = "x^3-1";
+            List<Summand> numerator = StringToSummand.Parse(numeratorStr);
+            numerator[0].Coefficient.Should().Be(1);
+            numerator[0].PolynomialDegree.Should().Be(3);
+            numerator[1].Coefficient.Should().Be(-1);
+            numerator[1].PolynomialDegree.Should().Be(0);
+        }
+
+        public void StringParse5()
+        {
+            string numeratorStr = "x^3-1";
+            List<Summand> numerator = StringToSummand.Parse(numeratorStr);
+            numerator[0].Coefficient.Should().Be(1);
+            numerator[0].PolynomialDegree.Should().Be(3);
+            numerator[1].Coefficient.Should().Be(-1);
+            numerator[1].PolynomialDegree.Should().Be(0);
+        }
+        
+        [TestMethod]
+        public void StringParse6()
+        {
+            string numeratorStr = "sin(x-1)";
+            List<Summand> numerator = StringToSummand.Parse(numeratorStr);
+            numerator[0].Coefficient.Should().Be(1);
+            numerator[0].Multiplicands.Count.Should().Be(1);
+            numerator[0].Multiplicands[0].Aparam.Should().Be(1);
+            numerator[0].Multiplicands[0].Bparam.Should().Be(-1);
+        }
+    }
+}
