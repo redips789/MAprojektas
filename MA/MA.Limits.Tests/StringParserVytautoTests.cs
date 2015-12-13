0246a93b15e513a7d02959cf6a67d97164befe3a
VytautasT <v.traskevicius@gmail.com>
2015-12-13 13:51:23 +0200

    String parser some more tests (mostly FAILING)

--- MA/MA.Limits.Tests/StringParserVytautoTests.cs
+++ MA/MA.Limits.Tests/StringParserVytautoTests.cs
@@ -52,5 +52,92 @@ namespace MA.Limits.Tests
 
         }
 
+
+        [TestMethod]
+        public void Parse_2()
+        {
+            string str = "3.5";
+
+            var result = StringToSummand.Parse(str);
+
+            result.Should().HaveCount(1);
+            result[0].PolynomialDegree.Should().Be(0);
+            result[0].Coefficient.Should().Be(3.5);
+            result[0].Multiplicands.Should().HaveCount(0);
+            result[0].SumsRaisedToPower.Should().HaveCount(0);
+        }
+
+        [TestMethod]
+        public void Parse_3()
+        {
+            string str = "-2.8";
+
+            var result = StringToSummand.Parse(str);
+
+            result.Should().HaveCount(1);
+            result[0].PolynomialDegree.Should().Be(0);
+            result[0].Coefficient.Should().Be(-2.8);
+            result[0].Multiplicands.Should().HaveCount(0);
+            result[0].SumsRaisedToPower.Should().HaveCount(0);
+        }
+
+        [TestMethod]
+        public void Parse_4()
+        {
+            string str = "x^2";
+
+            var result = StringToSummand.Parse(str);
+
+            result.Should().HaveCount(1);
+            result[0].PolynomialDegree.Should().Be(2);
+            result[0].Coefficient.Should().Be(1.0);
+            result[0].Multiplicands.Should().HaveCount(0);
+            result[0].SumsRaisedToPower.Should().HaveCount(0);
+        }
+
+        [TestMethod]
+        public void Parse_5()
+        {
+            string str = "(x+4)^(2/3)";
+
+            var result = StringToSummand.Parse(str);
+
+            result.Should().HaveCount(1);
+            result[0].PolynomialDegree.Should().Be(0);
+            result[0].Coefficient.Should().Be(1.0);
+            result[0].Multiplicands.Should().HaveCount(1);
+            result[0].Multiplicands[0].Should()
+                .Match<PowerFunction>(
+                    x =>
+                        MathHelper.AreApproximatelyEqual(x.Aparam, 1.0) &&
+                        MathHelper.AreApproximatelyEqual(x.Bparam, 4.0) &&
+                        x.PowerNumerator == 2 &&
+                        x.PowerDenominator == 3);
+
+            result[0].SumsRaisedToPower.Should().HaveCount(0);
+        }
+
+
+        [TestMethod]
+        public void Parse_6()
+        {
+            string str = "x^(2/3)";
+
+            var result = StringToSummand.Parse(str);
+
+            result.Should().HaveCount(1);
+            result[0].PolynomialDegree.Should().Be(0);
+            result[0].Coefficient.Should().Be(1.0);
+            result[0].Multiplicands.Should().HaveCount(1);
+            result[0].Multiplicands[0].Should()
+                .Match<PowerFunction>(
+                    x =>
+                        MathHelper.AreApproximatelyEqual(x.Aparam, 1.0) &&
+                        MathHelper.AreApproximatelyEqual(x.Bparam, 0.0) &&
+                        x.PowerNumerator == 2 &&
+                        x.PowerDenominator == 3);
+
+            result[0].SumsRaisedToPower.Should().HaveCount(0);
+        }
     }
 }
