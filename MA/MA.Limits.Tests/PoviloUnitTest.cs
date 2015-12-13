5fd2c6cd8c87707c58c9d44a163f28c8f85d1c85
redips789 <povilas.jegelevicius@gmail.com>
2015-12-13 13:33:49 +0200

    added string parser test

--- MA/MA.Limits.Tests/PoviloUnitTest.cs
+++ MA/MA.Limits.Tests/PoviloUnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using MA.Limits.Helpers;
 using MA.Limits.LimitsDomain;
@@ -15,41 +16,43 @@ namespace MA.Limits.Tests
     {
         [TestMethod]
         // (lim x->0) sin(5x) / x = 5
-        public void CalculatePoviloLimit1()
+        public void CalculatePoviloLimit1_StringParse()
         {
-            var numerator = new List<Summand>
-            {
-                new Summand
-                {
-                    Coefficient = 1.0,
-                    Multiplicands = new List<IElementaryFunction>
-                    {
-                        new Sine {Aparam = 5, Bparam = 0}
-                    }
-                }
-            };
-
-            var denominator = new List<Summand>
-            {
-                new Summand
-                {
-                    Coefficient = 1.0,
-                    PolynomialDegree = 1,
-                }
-            };
-
+            string numerator = "sin(5*x)";
+            string denominator = "x";
             var normalizedFunction = new NormalizedFunction
             {
-                Numerator = numerator,
-                Denominator = denominator
+                Numerator = StringToSummand.Parse(numerator),
+                Denominator = StringToSummand.Parse(denominator)
             };
-
+            normalizedFunction.Numerator.Count.Should().Be(1);
+            normalizedFunction.Denominator.Count.Should().Be(1);
+            normalizedFunction.Numerator.ToArray().First().Coefficient.Should().Be(1);
+            normalizedFunction.Numerator.ToArray().First().PolynomialDegree.Should().Be(0);
+            normalizedFunction.Numerator.ToArray().First().Multiplicands.ToArray().Length.Should().Be(1);
+            normalizedFunction.Numerator.ToArray().First().Multiplicands.ToArray().First().Aparam.Should().Be(5);
+            normalizedFunction.Denominator.ToArray().First().PolynomialDegree.Should().Be(1);
+            normalizedFunction.Denominator.ToArray().First().Coefficient.Should().Be(1);
             var result = LimitCalculator.CalculateLimit(normalizedFunction, 0);
-
             result.LimitResultType.Should().Be(LimitResultType.RealNumber);
             result.Value.Should().Be(5.0);
         }
 
+        [TestMethod]
+        public void CalculatePoviloLimit2_StringParse()
+        {
+            string numerator = "5*x-x^(1/2)";
+            List<Summand> summands = StringToSummand.Parse(numerator);
+            summands.First().Coefficient.Should().Be(5);
+            summands.First().PolynomialDegree.Should().Be(1);
+            summands[1].Coefficient.Should().Be(-1);
+            var i = summands[1].PolynomialDegree;
+            summands[1].PolynomialDegree.Should().Be(1);
+            summands[1].Multiplicands[0].Aparam.Should().Be(1);
+            summands[1].Multiplicands[0].Bparam.Should().Be(0);
+            //summands[1].
+            summands.Count.Should().Be(2);
+        }
         [TestMethod]
         // (lim x->1) 5x-x^0.5 = 4
         public void CalculatePoviloLimit2()
